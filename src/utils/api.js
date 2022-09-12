87b4fd0670ff5c267b4bd23d8c707c0557d1dfba
dwmorrell <davismorrell@gmail.com>
2022-09-12 17:17:05 -0600

    attaching API

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,8 +5,8 @@ import formatReservationTime from "./format-reservation-date";
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
- //const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://restaurant-res-backend-app.herokuapp.com";
- const API_BASE_URL = "http://localhost:5001"
+ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://restaurant-res-backend-app.herokuapp.com";
+ //const API_BASE_URL = "http://localhost:5001"
 
 /**
  * Defines the default headers for these functions to work with `json-server`
